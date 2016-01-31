eff25fed1ca4fb183d6af7b010b1becbac241e25
dragonmost <dragonmost@live.ca>
2016-01-30 20:44:14 -0500

    traduction et ajout de verification de compte

--- web/app/models/models.php
+++ web/app/models/models.php
@@ -8,35 +8,41 @@
  */
 class models
 {
+    //Se connecte à une base de donnée précise et renvoie la connexion
+    protected function DBConnection()
+    {
+        //Temporaire, mettre les bonne valeur
+        return new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'User', 'Passe');
+    }
 
     //Fonction qui permet de récupérer toute les informations d'une recherche
-    protected function BDRecherche($Commande){
-
+    protected function DBSearch($Command)
+    {
         //Récupère la connexion à la base de donnée
-        $BD = Connexion();
+        $pdo = DBConnection();
 
-        $Requete = $BD->prepare($Commande);
-        $Requete->execute();
-        $Resultat = $Requete->fetchAll(PDO::FETCH_ASSOC);
+        $request = $pdo->prepare($Command);
+        $request->execute();
+        $result = $request->fetchAll(PDO::FETCH_ASSOC);
 
-        $BD = null;
+        $pdo = null;
 
-        return $Resultat;
+        return $result;
     }
 
     //Fonction qui permet de récupérer la première information d'une recherche
-    protected function BDExecute($Commande){
-
+    protected function DBExecute($Command)
+    {
         //Récupère la connexion à la base de donnée
-        $BD = Connexion();
+        $pdo = DBConnection();
 
-        $Requete = $BD->prepare($Commande);
-        $Requete->execute();
-        $Resultat = $Requete->fetch(PDO::FETCH_ASSOC);
+        $request = $pdo->prepare($Command);
+        $request->execute();
+        $result = $request->fetch(PDO::FETCH_ASSOC);
 
-        $BD = null;
+        $pdo = null;
 
-        return $Resultat;
+        return $result;
     }
 
     //génération de token et vérification de doublons
@@ -52,7 +58,7 @@ class models
 
         $command = "SELECT Token FROM users WHERE Token=" + $token;
         //pas certain de ce que la BD renvoie si trouve rien
-        $result = $this->BDExecute($command);
+        $result = $this->DBSearch($command);
         if($result[0][0] == null)
             $this->TokenGen();
         return $token;
@@ -64,18 +70,18 @@ class models
         //verifier que "Type" cause pas de probleme
         $command = "INSERT INTO users (FirstName, LastName, Password, Email, Tel, Type, Token)";
         $values = " VALUES(" + $fName + "," + $lName + "," + md5($pw) + "," + $email + "," + $tel + "," + $type + "," + $this->TokenGen() + ")";
-        $this->BDExecute($command + $values);
+        $this->DBExecute($command + $values);
     }
 
     //connexion de l'utilisateur avec email et mot de passe
     public function Login($email, $pw)
     {
         $command = "SELECT Type, Token FROM users WHERE Email= " + $email + " Password= " + md5($pw);
-        $result = $this->BDExecute($command);
+        $result = $this->DBSearch($command);
         $_SESSION["token"] = $result[0][0];
         $_SESSION["type"] = $result[0][1];
 
-        //expire apres une journee et surtout le domaine
+        //expire apres une journee et sur tout le domaine
         setcookie("token", $result[0][0], time() + (86400 * 30), "/");
     }
 
@@ -83,8 +89,14 @@ class models
     public function TokenLogin($token)
     {
         $command = "SELECT Type FROM users WHERE Token = " + $token;
-        $result = $this->BDExecute($command);
+        $result = $this->DBSearch($command);
         $_SESSION["token"] = $token;
         $_SESSION["type"] = $result[0][0];
+    }
+
+    //valide le compte mettant la date de validation dans la bd
+    public function ValidateAccount($id)
+    {
+        $this->DBExecute("Update ValDate Set " + date('Y-m-d H:i:s') + " WHERE ID = " + $id);
     }
 }
