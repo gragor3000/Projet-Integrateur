77982d7db8e32a75ad5b607acd058ebda9c1f7c0
Michaël Légaré <gragor30@gmail.com>
2016-02-01 20:28:02 -0500

    finalisation de la bd et du model conceptuel

--- BD/pintegrateur.sql
+++ BD/pintegrateur.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Lun 01 Février 2016 à 22:38
+-- Généré le :  Mar 02 Février 2016 à 02:27
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -34,8 +34,23 @@ CREATE TABLE IF NOT EXISTS `entreprises` (
   `email` varchar(52) NOT NULL COMMENT 'addresse courriel',
   `account` int(11) NOT NULL COMMENT 'compte donnée',
   `status` tinyint(1) NOT NULL COMMENT '0= en attente 1= approuvée',
-  PRIMARY KEY (`ID`)
+  PRIMARY KEY (`ID`),
+  KEY `account` (`account`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='entreprises' AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `names`
+--
+
+CREATE TABLE IF NOT EXISTS `names` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'clé primaire',
+  `name` varchar(64) NOT NULL COMMENT 'nom de l''utilisateur',
+  `userID` int(11) NOT NULL COMMENT 'id de l''utilisateur',
+  PRIMARY KEY (`ID`),
+  KEY `userID` (`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='nom des utilisateurs' AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -57,7 +72,9 @@ CREATE TABLE IF NOT EXISTS `projects` (
   `status` tinyint(1) NOT NULL COMMENT '0=en attente 1= approuvée',
   `internID` int(11) NOT NULL COMMENT 'stagiaire affecté au projet',
   `entID` int(11) NOT NULL COMMENT 'entreprise du projet',
-  PRIMARY KEY (`ID`)
+  PRIMARY KEY (`ID`),
+  KEY `internID` (`internID`),
+  KEY `entID` (`entID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='projet de stage' AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -71,7 +88,9 @@ CREATE TABLE IF NOT EXISTS `ratings` (
   `score` int(11) NOT NULL COMMENT 'note d''appréciation',
   `internID` int(11) NOT NULL COMMENT 'clé étrangère du stagiaire',
   `projectID` int(11) NOT NULL COMMENT 'clé étrangère vers le projet',
-  PRIMARY KEY (`ID`)
+  PRIMARY KEY (`ID`),
+  KEY `internID` (`internID`),
+  KEY `projectID` (`projectID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='appréciation des projets' AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -85,9 +104,39 @@ CREATE TABLE IF NOT EXISTS `users` (
   `user` varchar(24) NOT NULL COMMENT 'nom d''utilisateur',
   `pw` varchar(24) NOT NULL COMMENT 'mot de passe ',
   `token` varchar(32) DEFAULT NULL COMMENT 'numéro d''authentification',
-  `group` int(11) NOT NULL COMMENT '0=coordonateur,1=entreprise,2= stagiaire',
+  `rank` int(11) NOT NULL COMMENT '0=coordonateur,1=entreprise,2= stagiaire',
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='utilisateurs' AUTO_INCREMENT=1 ;
+
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `entreprises`
+--
+ALTER TABLE `entreprises`
+  ADD CONSTRAINT `fk_account` FOREIGN KEY (`account`) REFERENCES `users` (`ID`);
+
+--
+-- Contraintes pour la table `names`
+--
+ALTER TABLE `names`
+  ADD CONSTRAINT `fk_NUserID` FOREIGN KEY (`userID`) REFERENCES `users` (`ID`);
+
+--
+-- Contraintes pour la table `projects`
+--
+ALTER TABLE `projects`
+  ADD CONSTRAINT `fk_PInternID` FOREIGN KEY (`internID`) REFERENCES `users` (`ID`),
+  ADD CONSTRAINT `fk_entID` FOREIGN KEY (`entID`) REFERENCES `entreprises` (`ID`);
+
+--
+-- Contraintes pour la table `ratings`
+--
+ALTER TABLE `ratings`
+  ADD CONSTRAINT `fk_RInternID` FOREIGN KEY (`internID`) REFERENCES `users` (`ID`),
+  ADD CONSTRAINT `fk_projectID` FOREIGN KEY (`projectID`) REFERENCES `projects` (`ID`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
