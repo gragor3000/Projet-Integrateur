7a941f80ed81d413d6df0260d1dff2da94b57d86
Baker <dragonmost@live.ca>
2016-02-17 09:09:44 -0500

    menu advisor

---
+++ web/app/views/advisor/menu.php
@@ -0,0 +1,41 @@
+<div class="navbar navbar-default navbar-static-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-ex-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"><span>420.AA | Gestion de Stage | Stagiaire</span></a>
+        </div>
+        <div class="collapse navbar-collapse" id="navbar-ex-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                       aria-expanded="false"><i class="fa fa-fw fa-user"></i> Nom de l'utilisateur <i
+                            class="fa fa-caret-down"></i></a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li>
+                            <a href="#"><i class="fa fa-fw fa-briefcase"></i> Voir tous les projets</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-fw fa-book"></i> Voir comptes</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="#"><i class="fa fa-fw fa-pencil"></i> Mes info</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="#"><i class="fa fa-fw fa-star-half-empty"></i> Evaluations</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="#"><i class="fa fa-fw fa-sign-out"></i> Déconnexion</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
