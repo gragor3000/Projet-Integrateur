fd834b63c2527078da43a389be2e5ebce23f0510
Guillaume <89gpb1010@gmail.com>
2016-02-24 15:06:02 -0500

    Debug d'un coordonnateur
    Modifications dans le menu de l'advisor.

--- web/app/views/advisor/menu.php
+++ web/app/views/advisor/menu.php
@@ -43,7 +43,7 @@
                             <a href="/advisor/assign"><i class="fa fa-fw fa-hand-o-right"></i> Panneau de jumelage</a>
                         </li>
                         <li>
-                            <a href="/advisor/review"><i class="fa fa-fw fa-comment"></i> Formulaires d'évaluations</a>
+                            <a href="/advisor/viewInterns"><i class="fa fa-fw fa-comment"></i> Formulaires d'évaluations</a>
                         </li>
 						<li>
                             <a href="/advisor/evalAdv"><i class="fa fa-fw fa-comment"></i> Évaluer un stagiaire</a>
