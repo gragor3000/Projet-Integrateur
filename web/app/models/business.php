fe07855478c1326a849b3da3639aa3ff5e549c65
Baker <dragonmost@live.ca>
2016-03-07 15:05:50 -0500

    Envoie de email

--- web/app/models/business.php
+++ web/app/models/business.php
@@ -1,14 +1,18 @@
 <?php
-class business extends Models {	
+
+class business extends Models
+{
 
     //Valider l'existence du email d'une compagnie
-    public function EmailExist($_email) {
-        $result = parent::DBQuery("SELECT email FROM business WHERE email='" . $_email."'");
+    public function EmailExist($_email)
+    {
+        $result = parent::DBQuery("SELECT email FROM business WHERE email='" . $_email . "'");
         return ($result != NULL);
     }
-	
+
     //Création d'un entreprise.
-    public function CreateBusiness($_address, $_city, $_tel, $_email, $_account) {
+    public function CreateBusiness($_address, $_city, $_tel, $_email, $_account)
+    {
         parent::DBExecute("INSERT INTO business (address, city, tel, email, userID)
 							VALUES(
 							'" . addslashes($_address) . "',
@@ -19,7 +23,8 @@ class business extends Models {
     }
 
     //Mise à jour des informations d'une entreprise.
-    public function UpdateBusiness($_id, $_address, $_city, $_tel, $_email) {
+    public function UpdateBusiness($_id, $_address, $_city, $_tel, $_email)
+    {
         parent::DBExecute("UPDATE business SET 
 							address = '" . addslashes($_address) . "',
 							city = '" . addslashes($_city) . "',
@@ -29,7 +34,8 @@ class business extends Models {
     }
 
     //Retourne toutes les entreprises.
-    public function ShowAllCie() {
+    public function ShowAllCie()
+    {
         $result = parent::DBSearch("SELECT * FROM cie ORDER BY city");
 
 
@@ -41,7 +47,8 @@ class business extends Models {
     }
 
     //Retourne une entreprise selon l'ID.
-    public function ShowCieByID($_businessID) {
+    public function ShowCieByID($_businessID)
+    {
         //Obtenir l'entreprise.
         $result = parent::DBQuery("SELECT * FROM business WHERE ID = " . $_businessID);
         //Obtenir le nom de l'entreprise.
@@ -50,19 +57,22 @@ class business extends Models {
 
         return new obj($result);
     }
-	
-	//Retourne une entreprise selon l'utilisateurID.
-    public function ShowCieByUserID($_userID) {
+
+    //Retourne une entreprise selon l'utilisateurID.
+    public function ShowCieByUserID($_userID)
+    {
         //Obtenir l'entreprise.
         $result = parent::DBQuery("SELECT * FROM business WHERE userID = " . $_userID);
-		
+
         //Obtenir le nom de l'entreprise.
         $name = parent::DBQuery("SELECT name FROM users WHERE ID = " . $_userID);
         $result['name'] = $name['name'];
         return new obj($result);
     }
+
     //Retourne les entreprises selon le status.
-    public function ShowCieByStatus($_status) {
+    public function ShowCieByStatus($_status)
+    {
         $result = parent::DBSearch("SELECT users.name, users.user, business.ID, business.address, business.city,
                                     business.tel, business.email FROM business INNER JOIN users
                                     ON users.ID = business.userID WHERE business.status = " . $_status);
@@ -75,21 +85,33 @@ class business extends Models {
         return $obj;
     }
 
+    //Génération d'un mot de passe.
+    public function PassGen() {
+        //Création du token.
+        $pass = md5(uniqid(rand(), TRUE));
+        //var_dump(substr($pass, -10));
+        return substr($pass, -10);
+    }
+
     //Valider une entreprise et envoyer un courriel.
-    public function AuthCie($_businessID) {
+    public function AuthCie($_businessID)
+    {
         parent::DBExecute("UPDATE business SET status=1 WHERE ID =" . $_businessID);
-		
+
         //Obtenir l'ID de compte et courriel.
         $result = parent::DBQuery("SELECT email,userID FROM business WHERE ID =" . $_businessID);
-		
+
         //Obtenir le nom d'utilisateur et mot de passe.
-        $user = parent::DBQuery("SELECT user,pw,name FROM users WHERE ID=" . $result['userID']);
+        $user = parent::DBQuery("SELECT ID,user,pw,name FROM users WHERE ID=" . $result['userID']);
+
+        $pw = $this->PassGen();
+        parent::DBExecute("UPDATE users SET pw ='" . md5($pw) .  "'WHERE ID=" . $user["ID"]);
 
         $msg = "Votre entreprise, " . $user["name"] . ", à été autorisé à soumettre des projets. Voici le nom
-        d'utilisateur et un mot de passe :\n Nom d'utilisateur : " . $user['user'] . "\n Mot de passe: " . $user['pw'];
+        d'utilisateur et un mot de passe :\n Nom d'utilisateur : " . $user['user'] . "\n Mot de passe: " . $pw;
 
         //Envoi du courriel de confirmation.
-        //mail($result['email'], $user["name"] . " Compte validée", $msg);
+        $mail = mail($result['email'], $user["name"] . " Compte validée", $msg);
     }
 
     //refuse une entreprise
@@ -97,30 +119,29 @@ class business extends Models {
     {
         $user = parent::DBQuery("SELECT business.userID, users.name, business.email FROM business
                                  INNER JOIN users ON business.userID = users.ID WHERE business.ID =" . $_businessID);
-        
-		$projects = parent::DBSearch("SELECT * FROM projects WHERE businessID =" .$_businessID);
-		
-		foreach($projects as $project)
-		{
-			    parent::DBExecute("DELETE FROM ratings WHERE projectID =" . $project['ID']);
-		}
-				
-		parent::DBExecute("DELETE FROM projects WHERE businessID =" . $_businessID);
-		
-		parent::DBExecute("DELETE FROM business WHERE ID =" . $_businessID);
-        parent::DBExecute("DELETE FROM users WHERE ID =" .$user["userID"]);
+
+        $projects = parent::DBSearch("SELECT * FROM projects WHERE businessID =" . $_businessID);
+
+        foreach ($projects as $project) {
+            parent::DBExecute("DELETE FROM ratings WHERE projectID =" . $project['ID']);
+        }
+
+        parent::DBExecute("DELETE FROM projects WHERE businessID =" . $_businessID);
+
+        parent::DBExecute("DELETE FROM business WHERE ID =" . $_businessID);
+        parent::DBExecute("DELETE FROM users WHERE ID =" . $user["userID"]);
 
         $msg = "Votre entreprise, " . $user["name"] . ", a été refusée.";
-        
+
         //Envoi du courriel de refus.
-        //mail($user["email"],$user["name"]."Compte refusé",$msg);		
+        $mail = mail($user["email"], $user["name"] . " Compte refusé", $msg);
     }
 
     //Supprimer une entreprise.
-    public function DeleteCie($_userID) 
-	{
+    public function DeleteCie($_userID)
+    {
         $business = parent::DBQuery("SELECT ID FROM business WHERE userID=" . $_userID);
-	
+
         $this->DenyCie($business['ID']);
     }
 
